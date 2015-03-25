7923a8a4d814139a33d068eeea2d0c98cec8d9d5
sunForest <sensun@live.cn>
2015-03-25 15:38:25 +0100

    start project & app

---
+++ avipost/functional_test/tests.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from django.test import LiveServerTestCase
+
+class NewVisitorTest(LiveServerTestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        pass
+        #self.browser.quit()
+
+    def test_home_page(self):
+        self.browser.get(self.live_server_url)
+        self.assertIn('Django', self.browser.title)
+
