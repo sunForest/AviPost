11be85c7fe13395743ad904d06433df73164ec80
sunForest <sensun@live.cn>
2015-04-19 10:32:22 +0200

    remove redundant app directory

--- postcards/models.py
+++
@@ -1,4 +0,0 @@
-from django.db import models
-
-class Postcard(models.Model):
-    content = models.TextField('Content')
--- postcards/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from rest_framework import viewsets
-from .models import Postcard
-
-class PostcardViewSet(viewsets.ModelViewSet):
-    queryset = Postcard.objects.all()
-
-# Create your views here.
