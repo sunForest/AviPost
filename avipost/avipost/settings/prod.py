9f34d8aafbf71d44a7423c111b5d0f2f91c1edad
sunForest <sensun@live.cn>
2015-07-03 00:17:01 +0200

    comment out db settings for prod

--- avipost/avipost/settings/prod.py
+++ avipost/avipost/settings/prod.py
@@ -15,15 +15,15 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 ALLOWED_HOSTS = ["52.16.214.13", "127.0.0.1", "localhost"]
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': 'postgres',
-        'USER': 'postcard_admin',
-        'PASSWORD': get_env_variable('DB_PASSWORD'),
-        'HOST': 'postcarddb.cqlnuay9niw1.eu-west-1.rds.amazonaws.com',
-        'PORT': '5432'
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.contrib.gis.db.backends.postgis',
+#         'NAME': 'postgres',
+#         'USER': 'postcard_admin',
+#         'PASSWORD': get_env_variable('DB_PASSWORD'),
+#         'HOST': 'postcarddb.cqlnuay9niw1.eu-west-1.rds.amazonaws.com',
+#         'PORT': '5432'
+#     }
+# }
 
 SECRET_KEY = get_env_variable('SECRET_KEY')
