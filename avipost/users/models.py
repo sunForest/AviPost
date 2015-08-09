d2b53f5649d6ce610ea813d1e474485e147ad8a7
sunForest <sensun@live.cn>
2015-08-09 21:03:24 +0200

    temp save

--- avipost/users/models.py
+++ avipost/users/models.py
@@ -1,10 +1,7 @@
 from django.db import models
-# from django.contrib.auth.models import AbstractBaseUser
-#
-# class OAuthUser(AbstractBaseUser):
-#     username = models.CharField('Username', max_length=75)
-#     provider = models.CharField('OAuth Provider', max_length=50)
-#     email = models.EmailField('Email')
-#
-#     USERNAME_FIELD = 'username'
+from django.contrib.auth.models import User
+
+class OAuthUser(models.Model):
+    user = models.OneToOneField(User)
+    oauth_provider = models.CharField('OAuth Provider', max_length=50)
 
--- avipost/users/views.py
+++ avipost/users/views.py
@@ -1,8 +1,8 @@
 from django.http import HttpResponse
-from django.contrib.auth import login
 from social.apps.django_app.utils import psa
 from .tools import get_access_token
-#from .models import OAuthUser
+from .models import OAuthUser
+
 
 @psa('social:complete')
 def register_by_access_token(request, backend):
@@ -11,12 +11,10 @@ def register_by_access_token(request, backend):
     user = request.backend.do_auth(token)
 
     if user:
-        # try:
-        #     localUser = OAuthUser.objects.get(username=user, provider=backend)
-        # except OAuthUser.DoesNotExist:
-        #
-        #     localUser = OAuthUser.objects.create(username=user, provider=backend, email=user.email)
-        # login(request, user)
+        try:
+            OAuthUser.objects.get(username=user, provider=backend)
+        except OAuthUser.DoesNotExist:
+            OAuthUser.objects.create(username=user, provider=backend, email=user.email)
         return get_access_token(user)
     else:
         return HttpResponse("error")
