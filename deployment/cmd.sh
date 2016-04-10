d42161298d85b52d51b9197c58af47ae7a5db535
sunForest <sensun@live.cn>
2016-04-10 10:41:07 +0200

    added db migration logic to deployment script

--- deployment/cmd.sh
+++ deployment/cmd.sh
@@ -2,15 +2,19 @@
 
 set -e
 
-if [ "$ENV" = 'ADMIN' ]; then
-   echo "setup api server ..."
-   export DJANGO_SETTINGS_MODULE="avipost.settings.prod"
-   python avipost/manage.py migrate 
-   python avipost/manage.py loaddata avipost/contrib/management/commands/usersDev.json
-   python avipost/manage.py loaddata avipost/contrib/management/commands/app.json
-   python avipost/manage.py loaddata avipost/contrib/management/commands/token.json
-   python avipost/manage.py loaddata avipost/contrib/management/commands/messenger.json
+if [ "$ENV" = 'SETUP' ]; then
+    echo "setup api server..."
+    export DJANGO_SETTINGS_MODULE="avipost.settings.prod"
+    python avipost/manage.py migrate 
+    python avipost/manage.py loaddata avipost/contrib/management/commands/usersDev.json
+    python avipost/manage.py loaddata avipost/contrib/management/commands/app.json
+    python avipost/manage.py loaddata avipost/contrib/management/commands/token.json
+    python avipost/manage.py loaddata avipost/contrib/management/commands/messenger.json
+elif ["$ENV" = 'UPDATE']; then
+    echo "migrating database..."
+    export DJANGO_SETTINGS_MODULE="avipost.settings.prod"
+    python avipost/manage.py migrate
 else
-   echo "running production server..."
-   supervisord -c /etc/supervisord.conf -n
+    echo "running production server..."
+    supervisord -c /etc/supervisord.conf -n
 fi
